865f77b970ea295d2c7d39276cf70220279ab431
samidi-b3 <samidib3@gmail.com>
2020-02-25 19:48:26 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo natus in recusandae consectetur fuga. Maxime inventore porro suscipit optio voluptate!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
